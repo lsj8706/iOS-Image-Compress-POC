9713cbb449bc5a2dc6ca9274c75faa0d43cb59f4
Lee SeJin <lsj8706@naver.com>
2024-04-10 19:45:12 +0900

    feat: 프로젝트 생성

---
+++ iOS-Image-Compress-POC/iOS-Image-Compress-POC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS-Image-Compress-POC
+//
+//  Created by Aiden.lee on 2024/04/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
